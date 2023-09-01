d5285526abd3d50deae10ea6f7b3f1b5dde89ee4
tsov09 <tsovinarsafaryan147@gmail.com>
2023-09-02 01:11:59 +0400

    compare bits' values and get binary code of two strings' sum

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
+
 int find_nth_bit(int number, int n) {
 	if (n >= 0 && n <= 8 * sizeof(int)) {
-		std::cout << "10th bit of " << number << " is " << (number >> n) << "." << std::endl; //this line is for demonstartion, need to be removed
+		std::cout << "10th bit of " << number << " is " << ((number >> n) & 1) << "." << std::endl; //this line is for demonstartion, need to be removed
 		return number >> n;
 	}
 	else {
@@ -20,26 +23,88 @@ int find_single_element(std::vector<int> nums) {
 	return single;
 }
 
+std::string get_binary_code_of_char(char ch) {
+	int x = ch + 0;
+	std::string code;
+	int indicator = 128;
+
+	for (int i = 0; i < 8 * sizeof(ch); i++) {
+		(indicator & x) == 0 ? (code += '0') : (code += '1');
+		indicator = indicator >> 1;
+	}
+	return code;
+}
+
+std::string get_binary_code_of_string(std::string str) {
+	std::string code;
+	for (int i = 0; i < str.length(); i++) {
+		code += get_binary_code_of_char(str[i]);
+	}
+	std::cout << code << std::endl;
+	return code;
+}
+
+std::string get_binary_code_of_two_strings_sum(std::string str_1, std::string str_2) {
+	return get_binary_code_of_string(str_1 + str_2);
+}
+
 int main() {
 
-	try {
-		find_nth_bit(1025, 10);
-		find_nth_bit(102, 10);
-		//find_nth_bit(102, 35);
-	}
-	catch (int x) {
-		std::cout << "The number is out of range of bit's count in int type." << std::endl;
+	//try {
+	//	find_nth_bit(3072, 10);
+	//	find_nth_bit(102, 10);
+	//	//find_nth_bit(102, 35);
+	//}
+	//catch (int x) {
+	//	std::cout << "The number is out of range of bit's count in int type." << std::endl;
+	//}
+
+	//std::vector<int> nums { 2, 7, 8, 5, 4, 8, 5, 2, 4 };
+	//
+	//std::cout << "Vector: { ";
+	//for (int i = 0; i < nums.size(); i++) {
+	//	std::cout << nums[i] << " ";
+	//}
+	//std::cout << "}" << std::endl;
+
+	//std::cout << find_single_element(nums) << " hasn't duplicate in vector." << std::endl;
+
+
+	std::cout << std::endl;
+
+	int a = 20119574;
+	int b = 1;
+	int count = 0;
+
+
+	for (int i = 0; i < 8 * sizeof(a); i++) {
+		if (a & b) {
+			count++;
+			if (count > 4 * sizeof(a)) {
+				break;
+			}
+		}
+		b = b << 1;
 	}
 
-	std::vector<int> nums { 2, 7, 8, 5, 4, 8, 5, 2, 4 };
+	std::cout << "Count of 1 in binary code of integer " << a << " is ";
+	if (count > 4 * sizeof(a)) {
+		std::cout << "greater than ";
+	}
+	else if (count < 4 * sizeof(a)) {
+		std::cout << "less than ";
+	}
+	else {
+		std::cout << "equal to ";
+	}
+	std::cout << " count of 0." << std::endl;
+	std::cout << std::endl;
+
+	std::string str_1("Hello ");
+	std::string str_2("World!");
+	std::cout << "Binary code of '" <<  str_1 << "' and '" << str_2 << "' strings sum is :" << std::endl;
+	std::string bin_code = get_binary_code_of_two_strings_sum(str_1, str_2);
+	std::cout << std::endl;
 	
-	std::cout << "Vector: { ";
-	for (int i = 0; i < nums.size(); i++) {
-		std::cout << nums[i] << " ";
-	}
-	std::cout << "}" << std::endl;
-
-	std::cout << find_single_element(nums) << " hasn't duplicate in vector.";
-
 	return 0;
 }
